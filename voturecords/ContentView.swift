7b0ad7cd44af313020c05081748946f62d985359
Yannick Peysale <yannick.peysale@gmail.com>
2020-09-07 11:58:54 +0200

    Initial Commit

---
+++ voturecords/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  voturecords
+//
+//  Created by Yannick Peysale on 07/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ voturecords/voturecordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  voturecordsApp.swift
+//  voturecords
+//
+//  Created by Yannick Peysale on 07/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct voturecordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
